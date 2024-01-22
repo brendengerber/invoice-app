6dab8bb227a62ae614191240123e23463bff306e
Brenden Gerber <10082638+brendengerber@users.noreply.github.com>
2024-01-22 15:32:22 -0600

    Add some more session logic to server.js

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const helmet = require('helmet');
 const {config} = require('./config.js');
 const cors = require('cors');
 require('dotenv').config();
+const session = require('express-session');
+const passport = require('./config/passport.js');
 
 const app = express();
 
@@ -22,9 +24,31 @@ app.disable('x-powered-by');
 //Parses request bodies to json
 app.use(express.json());
 
+//Initializes passport to be used on all routes
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Allows images from github to load
+app.use(function(req, res, next) {
+  res.setHeader("Content-Security-Policy", "img-src self https://avatars.githubusercontent.com");
+  return next();
+});
+
+//Express session set up to be used on all routes
+//***********Need to set up storrage */
+//***********Add secure when https is set up and samesite? */
+app.use(
+  session({
+    secret: EXPRESS_SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 *60 * 24 }
+  })
+);
+
 //Mounts the api router
 const apiRouter = require('./api-router.js');
-app.use('/api', apiRouter);
+app.use('/', apiRouter);
 
 //Sets the port and starts the server
 const PORT = process.env.PORT || config.port;
