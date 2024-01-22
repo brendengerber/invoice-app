cf9383b1292e15e7a6b9b07a09e7aafd21347d11
Brenden Gerber <10082638+brendengerber@users.noreply.github.com>
2024-01-22 15:35:12 -0600

    Add first route to authRouter.js

---
+++ routers/auth-router.js
@@ -0,0 +1,10 @@
+//Imports necessary modules
+const express = require('express');
+require('dotenv').config();
+const passport = require('./config/passport.js');
+
+//Creates the router
+const authRouter = express.Router();
+
+//Authenticates via github oauth
+authRouter.post('/github', passport.authenticate('github', { failureRedirect: process.env.LOGIN_URL, failureMessage: true }));
