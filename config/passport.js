8e159b0711ccdc3c7b01f525fbd427042fa33c66
Brenden Gerber <10082638+brendengerber@users.noreply.github.com>
2024-02-01 21:42:18 -0600

    Add notes to passport.js

--- config/passport.js
+++ config/passport.js
@@ -14,10 +14,13 @@ passport.use(
             callbackURL: `${process.env.URL}/auth/github/callback`
         },
         //This portion is called if there is no open session (i.e. the browser does not send a cookie)
-        //It then associates the github profile to a user in the application database by matching the provider and id in the profile to provider and remoteId in the app database
-        //Or creates the user if it does not exist using the github profile (assigning the app specific uuid used to look up data in other tables along with other relevant data like email)
+        //It then associates the github profile to a user in the application database 
+        //This is done by matching the provider and id in the profile to provider and remoteId in the app database via an sql query
+        //Or if one doesnt exist it creates the user via an sql query, using the github profile for the column data (assigning the app specific uuid used to look up data in other tables along with other relevant data like email)
+        //This user is then attatched to req.user
         //It then creates a session by assigning and storing a cookie in the session database 
-        //Then serializes the User for subsequent requests while the session is ongoing
+        //Then it serializes the User for subsequent requests while the session is ongoing by saving the req.user.id to the session
+        //This id can then be used by deserialize when it recieves the session cookie to look up and attatch the full user object to req.user
         //Note that req.user will now be the full user from the app database not the oauth profile
         (accessToken, refreshToken, profile, done) => {
             //Finds a user based on the provider and providerId/remoteId from the profile obtained by Oauth
@@ -42,8 +45,8 @@ passport.use(
 passport.serializeUser((user, done) => {
     done(null, user.id);
 });
-//Deserializes the user upon subsequent requests with an open session by looking up the cookie in the session database
-//Uses the UUID stored to the session database by serializeUser to look up the full user from the app users database table and attatching it to req.User
+//Deserializes the user upon subsequent requests with an open session by matching the cookie to a session in the session database
+//Uses the UUID stored in the session by serializeUser to look up the full user from the app users database and attatch it to req.user
 passport.deserializeUser((id, done) => {
     db.user.findByPk(id)
     .then(result => {
