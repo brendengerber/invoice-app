5bbe3df7a514221c9dd83265815ad58372b68964
Brenden Gerber <10082638+brendengerber@users.noreply.github.com>
2024-02-05 15:14:16 -0600

    Add authorization middleware

---
+++ middleware/authorization-middleware.js
@@ -0,0 +1,50 @@
+//This function can be used to assign the roles that are allowed to access a route, and check if req.user has any of those roles
+//Roles is an array of roles allowed to access the resource on a specific route (admin, group memeber, owner)
+//Resource is a string for the req property that will be checked
+
+//New roles can be added by adding additional if statements and their associated checking logic 
+//New roles should return next() if the user is found to be authorized and do nothing if they are found unauthorized (as the function throws an unauthorized error by default)
+//This prevents other checks from being run once at least one valid authorized role is found
+function authorizeUser(roles, resource){
+    return authorizeUser[roles, resource] || (authorizeUser[roles, resource] = (req, res, next) => {
+        try{
+            //Checks if the logged in user has the admin role
+            if(roles.includes('admin')){
+                if(req.user.admin){
+                    return next()
+                }
+            }
+
+            //Checks if the logged in user is the owner of the resource by comparing req.user.id to resource.userId
+            if(roles.includes('owner')){
+                //Handles cases where the resource is an array and checks ownership of all resources in the array
+                //If ownership is found false on any resource, the check fails, and authorized is left as false
+                if(Array.isArray(req[resource])){
+                    let allAuthorized;
+                    for(let item of req[resource]){
+                        if(req.user.id !== item.userId){
+                            allAuthorized = false;
+                            break;
+                        } 
+                    }
+                    if(allAuthorized !== false){
+                        return next()
+                    }
+                //Handles cases of a single resource
+                }else if(req.user.id === req[resource].userId){
+                    authorized = true;
+                }
+            }
+
+            //If no valid authorization is found a 401 error is thrown by default
+            throw new Error("Access denied", {statusCode: 401});
+            
+        }catch(err){
+            next(err);
+        }
+    })
+};
+
+module.exports = {
+    authorizeUser
+};
--- routers/invoice-router.js
+++ routers/invoice-router.js
@@ -2,14 +2,16 @@
 const express = require('express');
 require('dotenv').config();
 const {ensureAuthenticated} = require('../middleware/authentication-middleware.js');
-const {getUserInvoices, getUserInvoiceById} = require('../middleware/invoice-middleware.js')
+const {getUserInvoices, getUserInvoiceById} = require('../middleware/invoice-middleware.js');
+const {authorizeUser} = require('../middleware/authorization-middleware.js')
+
 
 //Creates the router
 const invoiceRouter = express.Router();
 
 //Gets all invoices associated with an authenticated User
-invoiceRouter.get('/all', ensureAuthenticated, getUserInvoices, (req, res, next) => {
-
+invoiceRouter.get('/all', ensureAuthenticated, getUserInvoices, authorizeUser(['owner', 'admin'], 'invoices'), (req, res, next) => {
+    res.status(200).send(req.invoices);
 })
 
 //Gets an invoice associated with an authenticated User by Id
