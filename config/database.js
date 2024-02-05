bf26d9b5f6a31380e8279ab98adb9a4ca0934d11
Brenden Gerber <10082638+brendengerber@users.noreply.github.com>
2024-02-05 10:59:55 -0600

    Remove raw: true from database config as it conflicts with eager loading

--- config/database.js
+++ config/database.js
@@ -4,10 +4,6 @@ const { Sequelize } = require('sequelize');
 //Creates amd exports the connected/instantiated Sequelize instance
 //Additional configuration can be done here to specify different databases for development, testing, and production using the NODE_ENV variable if desired
 module.exports = new Sequelize(process.env.DATABASE_URL, {
-    query: {
-        //Sets to return queries only, without metadata
-        raw: true
-    },
     logging: false
 });
 
