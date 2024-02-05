32bba6e5ab4871c0182f3a1280858b57e0f483a5
Brenden Gerber <10082638+brendengerber@users.noreply.github.com>
2024-02-05 15:49:04 -0600

    Change resource does not exist message to access denied so as to not give too much information away to potential bad actors

--- utilities/database-utilities.js
+++ utilities/database-utilities.js
@@ -1,9 +1,9 @@
-
 //Unwraps the results from a sequelize query to remove metadata and allow access to results at the top level of the object
 function unwrapQueryResults(results){
     //Throws an error in case of an unfound result
     if(results === undefined || results === null || results.length === 0){
-        throw new Error("The requested resources does not exist.", {statusCode: 404});
+        //Standard Access denied message here so as to not disclose if a resource exists or not to potential bad actors
+        throw new Error("Access denied", {statusCode: 401});
     }
     let unwrappedResults;
     if(Array.isArray(results)){
