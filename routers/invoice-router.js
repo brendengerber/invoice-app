3eb61109b8391c57bbe929c9686f844f73d4c799
Brenden Gerber <10082638+brendengerber@users.noreply.github.com>
2024-02-05 15:47:49 -0600

    Finish get invoice by id route

--- routers/invoice-router.js
+++ routers/invoice-router.js
@@ -3,11 +3,14 @@ const express = require('express');
 require('dotenv').config();
 const {ensureAuthenticated} = require('../middleware/authentication-middleware.js');
 const {getUserInvoices, getUserInvoiceById} = require('../middleware/invoice-middleware.js');
-const {authorizeUser} = require('../middleware/authorization-middleware.js')
-
+const {authorizeUser} = require('../middleware/authorization-middleware.js');
+const {checkParamId} = require('../middleware/checking-middleware.js');
 
 //Creates the router
 const invoiceRouter = express.Router();
+
+//Validates and sanitizes all id parameters
+invoiceRouter.param('id', checkParamId('invoiceId'));
 
 //Gets all invoices associated with an authenticated User
 invoiceRouter.get('/all', ensureAuthenticated, getUserInvoices, authorizeUser(['owner', 'admin'], 'invoices'), (req, res, next) => {
@@ -15,8 +18,8 @@ invoiceRouter.get('/all', ensureAuthenticated, getUserInvoices, authorizeUser(['
 })
 
 //Gets an invoice associated with an authenticated User by Id
-invoiceRouter.get('/:id', ensureAuthenticated, getUserInvoices, (req, res, next) => {
-
+invoiceRouter.get('/:id', ensureAuthenticated, getUserInvoiceById, authorizeUser(['owner', 'admin'], 'invoice'), (req, res, next) => {
+    res.status(200).send(req.invoice);
 })
 
 
