219a5b09e4921b2d930ddab16c329bf9759cd538
Brenden Gerber <10082638+brendengerber@users.noreply.github.com>
2024-02-07 15:43:44 -0600

    Update the addPropertyToDatabaseObject utility to be more generic

--- utilities/database-utilities.js
+++ utilities/database-utilities.js
@@ -23,21 +23,41 @@ function checkForEmptyResults(results){
     }
 };
 
-//Adds userId property to database objects sent via form submits
-//Recursive function will check nested objects from associations as well
-function addUserIdToDatabaseObject(object, id){
-    object.userId = id;
-    for(key in object){
+//Adds property to database objects sent via form submits
+//Recursive function will add the property to nested objects from associations as well
+//Object is the object to update, newKey is a string for the key to be added, and newValue is the newKey's corresponding value
+//Useful for adding ids, userIds, invoiceIds, etc to objects with associations
+function addPropertyToDatabaseObject(object, newKey, newValue){
+    
+    if(object && typeof object === 'object' && !Array.isArray(object)){
+        object[newKey] = newValue;
+    }else if(object && Array.isArray(object)){
+        for(let index of object){
+            addPropertyToDatabaseObject(index, newKey, newValue)
+        }
+    }
+    
+    for(let key in object){
         if(object[key] && typeof object[key] === 'object' && !Array.isArray(object[key])){
-            addUserIdToDatabaseObject(object[key], id);
-        }else if(object && Array.isArray(object[key])){
+            addPropertyToDatabaseObject(object[key], newKey, newValue);
+        }else if(object[key] && Array.isArray(object[key])){
             for(let index of object[key]){
-                addUserIdToDatabaseObject(index, id);
+                addPropertyToDatabaseObject(index, newKey, newValue);
             }
         }
     }
     return object
 }
+
+
+let testObject = {
+    property1: {test: "test"},
+    property2: [{test2: "test 2"}, [{test3: "test 3"}]],
+    property3: [[[{test4: "test4"}]]]
+}
+
+console.dir(addPropertyToDatabaseObject(testObject, "id", 5), {depth: null})
+
 
 //Displays full error message when server is set to development
 //Hides full error message when server is set to production to avoid providing information to potential bad actors
@@ -60,6 +80,6 @@ function updateDatabaseObject(object, objectUpdates){
 module.exports = {
     unwrapQueryResults,
     checkForEmptyResults,
-    addUserIdToDatabaseObject,
+    addPropertyToDatabaseObject,
     processQueryError
 };
