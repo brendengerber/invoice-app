58995e54d482a13c3bff67f069bfbd13e140dc78
Brenden Gerber <10082638+brendengerber@users.noreply.github.com>
2024-02-10 10:38:01 -0600

    Reconfigure authentication rediercts

--- api-router.js
+++ api-router.js
@@ -16,9 +16,6 @@ apiRouter.use('/authentication', authRouter);
 const userRouter = require('./routers/user-router.js');
 apiRouter.use('/user', userRouter);
 
-const oauthRouter = require('./routers/oauth-router.js');
-apiRouter.use('/', oauthRouter);
-
 //Handles all unhandled errors
 apiRouter.use((err, req, res, next) => {
     if(!err.status){
--- routers/authentication-router.js
+++ routers/authentication-router.js
@@ -12,9 +12,18 @@ authRouter.get('/github', passport.authenticate('github', {scope: ['user']}));
 //Callback route which Github will call following the authentication attempt
 //User will be redirected based on the success of the authentication attempt
 authRouter.get('/github/callback', passport.authenticate('github', {
-    failureRedirect: `/failure`,
-    successRedirect: `/success`
+    failureRedirect: `/authentication/failure`,
+    successRedirect: `/authentication/success`
 }));
+
+//Redirects callback redirects from Oauth to the front end pages not hosted on the api subdomain
+oauthRouter.get('/failure', (req, res, next) => {
+    res.status(401).send();
+});
+
+oauthRouter.get('/success', (req, res, next) => {
+    res.status(200).send(req.user);
+});
 
 //Logs out of any open passport session
 authRouter.post('/logout', (req, res, next) => {
