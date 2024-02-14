e7e45ac77b738d67aea1cd93f2eb52f18aca700d
Brenden Gerber <10082638+brendengerber@users.noreply.github.com>
2024-02-14 14:22:34 -0600

    Add config vars to database.js to return numbers as numbers instead of strings in queries

--- config/database.js
+++ config/database.js
@@ -4,6 +4,7 @@ const { Sequelize } = require('sequelize');
 //Creates amd exports the connected/instantiated Sequelize instance
 //Additional configuration can be done here to specify different databases for development, testing, and production using the NODE_ENV variable if desired
 module.exports = new Sequelize(process.env.DATABASE_URL, {
-    logging: false
+    logging: false,
+    dialectOptions: { decimalNumbers: true }
 });
 
