b5555c86c76413740877ae6dc9d5c04e0bf82f81
Brenden Gerber <10082638+brendengerber@users.noreply.github.com>
2024-02-24 12:16:17 -0600

    Update CORS for testing

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.use(
 // ***************uncomment below???
 app.use(
   cors({
-    origin: "http://localhost:3000", // allow to server to accept request from different origin
+    origin: "*", // allow to server to accept request from different origin
     // methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     // credentials: true // allow session cookie from browser to pass through
   })
