7a561a25fda9c631bde23ee3dbcd6718c1f51d99
Brenden Gerber <10082638+brendengerber@users.noreply.github.com>
2024-03-03 15:48:51 -0600

    Update cors settings

--- server.js
+++ server.js
@@ -31,9 +31,10 @@ app.use(express.urlencoded({extended: true}));
 // ***************uncomment below???
 app.use(
   cors({
-    origin: "*", // allow to server to accept request from different origin
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true // allow session cookie from browser to pass through
+    // origin: "*", // allow to server to accept request from different origin
+    // methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true, // allow session cookie from browser to pass through
+    origin: true
   })
 );
 
