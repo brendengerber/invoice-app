2b992c07f8dd22d9a26638d963976b557632fca6
Brenden Gerber <10082638+brendengerber@users.noreply.github.com>
2024-03-03 18:24:39 -0600

    Update session settings...again

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.use(
     saveUninitialized: false,
     samesite: 'none',
     secure: true,
-    // httpOnly: true,
+    httpOnly: false,
     secure: false,
     // cookie: { maxAge: 1000 * 60 *60 * 24, httpOnly: false },
     cookie: { maxAge: 1000 * 60 *60 * 24 },
