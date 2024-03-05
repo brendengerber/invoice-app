f3282d10dc53f4a0549e2b0a4a817ab3305581f7
Brenden Gerber <10082638+brendengerber@users.noreply.github.com>
2024-03-05 16:23:22 -0600

    Refactor authentication router to use env var for redirects

--- routers/authentication-router.js
+++ routers/authentication-router.js
@@ -6,31 +6,14 @@ const passport = require('../config/passport.js');
 //Creates the router
 const authRouter = express.Router();
 
-//*****add the frontend base url to .env.example and dokku config files and refactor below to use it */
-//*********redirect to a success route that sends the user object and redirects to dashboard? */
-//Sets the redirect URLs based on if the current environment is development or production
-let failureRedirectURL;
-let successRedirectURL;
-let logoutRedirectURL;
-
-if(process.env.NODE_ENV === 'production'){
-    failureRedirectURL = `${process.env.FRONT_END_URL}/login/`;
-    successRedirectURL = `${process.env.FRONT_END_URL}/dashboard/`;
-    logoutRedirectURL = `${process.env.FRONT_END_URL}/login/`;
-}else if(process.env.NODE_ENV === 'development'){
-    failureRedirectURL = `http://localhost:3000/login/`;
-    successRedirectURL = `http://localhost:3000/dashboard/`;
-    logoutRedirectURL = `http://localhost:3000/login/`;
-}
-
 //Authenticates via github oauth
 authRouter.get('/github', passport.authenticate('github', {scope: ['user']}));
 
 //Callback route which Github will call following the authentication attempt
 //User will be redirected based on the success of the authentication attempt
 authRouter.get('/github/callback', passport.authenticate('github', {
-    failureRedirect: failureRedirectURL,
-    successRedirect: successRedirectURL
+    failureRedirect: `${process.env.FRONT_END_URL}/login/`,
+    successRedirect: `${process.env.FRONT_END_URL}/dashboard/`
 }));
 
 //Logs out of any open passport session
@@ -39,7 +22,7 @@ authRouter.post('/logout', (req, res, next) => {
         if(err){
             return next(err);
         }
-        res.redirect(logoutRedirectURL);
+        res.redirect(`${process.env.FRONT_END_URL}/login/`);
     })
 });
 
