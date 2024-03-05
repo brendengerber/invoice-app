e7afee7251e164a1606c7df5651a65f040d0762a
Brenden Gerber <10082638+brendengerber@users.noreply.github.com>
2024-03-05 13:47:00 -0600

    Update session for production

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.use(
     saveUninitialized: false,
     httpOnly: false,
     cookie: { 
-      sameSite: "none",
+      sameSite: "lax",
       secure: "true",
       maxAge: 1000 * 60 *60 * 24
     },
