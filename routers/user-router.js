c0be25ddd80797031fffaac918cb1fb208dcbfd1
Brenden Gerber <10082638+brendengerber@users.noreply.github.com>
2024-03-09 10:39:31 -0600

    Add ensure authenticated to user route

--- routers/user-router.js
+++ routers/user-router.js
@@ -1,11 +1,13 @@
 //Imports necessary modules
 const express = require('express');
 require('dotenv').config();
+const {ensureAuthenticated} = require('../middleware/authentication-middleware.js');
+
 
 //Creates the router
 const userRouter = express.Router();
 
-userRouter.get('/', (req, res, next) => {
+userRouter.get('/', ensureAuthenticated, (req, res, next) => {
     res.status(200).send(req.user);
 });
 
